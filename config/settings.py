00fe6d2798fca36f7ba84da9e6438885efee9623
erkin7333 <erkinnajimov90@gmail.com>
2023-01-09 14:14:26 +0500

    Jamoa uchun Model yaratildi

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.0:8000',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,11 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
+    'lead',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ lead/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+"""Yetakchilar uchum model"""
+class Lead(models.Model):
+    NEW = 'Yangi'
+    CONTACTED = 'ALOQA QILGAN'
+    INPROGRESS = 'JARAYONDA'
+    LOST = "YO'QOTILGAN"
+    WON = 'YUTUQ'
+    CHOICES_STATUS = (
+        (NEW, 'Yangi'),
+        (CONTACTED, 'ALOQA QILGAN'),
+        (INPROGRESS, 'JARAYONDA'),
+        (LOST, "YO'QOTILGAN"),
+        (WON, 'YUTUQ')
+    )
+
+    LOW = "PAST"
+    MEDIUM = 'OʻRTA'
+    HIGH = 'YUQORI'
+    CHOICES_PRIORITY = (
+        (LOW, "PAST"),
+        (MEDIUM, 'OʻRTA'),
+        (HIGH, 'YUQORI')
+    )
+
+    company = models.CharField(max_length=255, verbose_name='Kompaniya')
+    contact_person = models.CharField(max_length=255, verbose_name="Shaxs bn bog'lanish")
+    email = models.EmailField(verbose_name="Elektron manzil")
+    phone = models.CharField(max_length=20, verbose_name="Telefon nomer")
+    website = models.CharField(max_length=255, blank=True, null=True, verbose_name="Vebsayit")
+    confidence = models.IntegerField(blank=True, null=True)
+    estimated_value = models.IntegerField(blank=True, null=True, verbose_name='Taxminiy qiymat')
+    status = models.CharField(max_length=50, choices=CHOICES_STATUS, default=NEW)
+    priority = models.CharField(max_length=50, choices=CHOICES_PRIORITY, default=MEDIUM)
+    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_ad = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.company
+
+    class Meta:
+        verbose_name = "Lead"
